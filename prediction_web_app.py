339820945613ca862bf3bd72ca6dfc3230fe8a1a
Your Name <you@example.com>
2023-10-21 20:31:56 +0530

    modeified prediction web app by adding sql query

--- prediction_web_app.py
+++ prediction_web_app.py
@@ -4,9 +4,12 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from src.pipeline.prediction_pipeline import PredictionPipeline
+import pymysql
 
 preprocessor =pickle.load(open('artifacts\data_transformation\preprocessor.pkl', 'rb'))
 model = pickle.load(open('artifacts\model_trainer\model.pkl', 'rb'))
+a = pymysql.connect(user='root',host='127.0.0.1',passwd='mrinal',db='patient_condition')
+cur = a.cursor()
                                 
                                 
 def main():
@@ -28,7 +31,19 @@ def main():
     if st.button('Predict Patient condition'):
         obj = PredictionPipeline()
         prediction=obj.predict(data=df)
-        
+        if prediction[0] == "Normal":
+            cur.execute('insert into Normal(P_incidence,P_tilt,L_angle,S_slope,P_radius,S_Degree,Class) values(%s,%s,%s,%s,%s,%s,%s)',(P_incidence,P_tilt,L_angle,S_slope,P_radius,S_Degree,prediction[0]))
+            a.commit()
+            a.close()
+        elif prediction[0] == 'Type_H':
+            cur.execute('insert into Type_H(P_incidence,P_tilt,L_angle,S_slope,P_radius,S_Degree,Class) values(%s,%s,%s,%s,%s,%s,%s)',(P_incidence,P_tilt,L_angle,S_slope,P_radius,S_Degree,prediction[0]))
+            a.commit()
+            a.close()
+        else:
+            cur.execute('insert into Type_S(P_incidence,P_tilt,L_angle,S_slope,P_radius,S_Degree,Class) values(%s,%s,%s,%s,%s,%s,%s)',(P_incidence,P_tilt,L_angle,S_slope,P_radius,S_Degree,prediction[0]))
+            a.commit()
+            a.close()
+
     st.success(prediction[0])
     
 if __name__ == '__main__':
